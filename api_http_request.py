7717a9dccedd75586a46f5120615910c7c6e6569
Luis Gallegos <luisgallegosxt@gmail.com>
2024-05-26 10:54:08 -0500

    Update api_http_request.py
    Update to prefect2

--- api_http_request.py
+++ api_http_request.py
@@ -1,11 +1,18 @@
-from prefect import task, Flow
-import prefect
+from prefect import task, flow, get_run_logger
+from prefect.deployments import Deployment
 from datetime import datetime, timedelta
 
 import requests
 import json
 import pandas as pd
 
+from dotenv import load_dotenv
+import os
+
+env_path = '/path/to/env/.env'
+load_dotenv(dotenv_path=env_path)
+
+api_key=os.getenv("API_KEY")
 
 def get_response_count(api_key):
 	"""Get the responses count per request"""
@@ -34,20 +41,18 @@ def get_responses(api_key, idx):
 
 
 def transform_response(row):
-	
 	row_dumps_resp = json.dumps(row['responseSet'])
 	row_list_resp = json.loads(row_dumps_resp)
 
 	for resp in row_list_resp:
         ## do any data treatment and create a new column with your result
 		row['any_column_name'] = 'any_treatment_result'
-
 	return row
 
 
 @task
 def get_responses():
-	logger = prefect.context.get("logger")
+	logger = get_run_logger()
 	logger.info("Start api call")
 	total_df = pd.DataFrame()
 
@@ -58,8 +63,7 @@ def get_responses():
 		api_resp = get_responses(api_key, idx + 1)
 		responses_dict = api_resp['response']
 		df = pd.DataFrame.from_dict(responses_dict)
-		
-		total_df = total_df.append(df)
+		total_df = pd.concat([total_df, df])
 		logger.info('Success'+ str(idx))
 
 	return total_df
@@ -85,14 +89,22 @@ def extract_response_task(df):
 	
 	return df_output
 
+@flow
+def api_http_request():
 
-with Flow("api_http_request") as flow:
-
-	api_key = API_KEY
+	api_key = api_key
 	
 	get_responses = get_responses()
 
 	extract_response_task = extract_response_task(get_responses)
 
+deployment = Deployment.build_from_flow(
+    flow = api_http_request,
+    name = "api_http_request",
+	path = '/path/to/code/',
+	work_queue_name = 'default',
+	work_pool_name = 'default-agent-pool',
+	tags=['api']
+)
+deployment.apply()
 
-flow.register(project_name="apis")
